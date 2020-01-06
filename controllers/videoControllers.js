4adf7e8655a05e5ff3fb75d269a45eb46aca6518
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-01-06 17:55:57 +0900

    3.1

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,7 @@
-import { videos } from "../db"
-import routes from "../routes"
+import routes from "../routes";
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos })};
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   const {
@@ -15,13 +15,11 @@ export const getUpload = (req, res) =>
 
 export const postUpload = (req, res) => {
   const {
-    body: {
-      file, title, description
-    }
+    body: { file, title, description }
   } = req;
   /// To Do: Upload and save video
-  res.redirect(routes.videoDetail(324393))
-}
+  res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
