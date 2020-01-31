9da14a85c696d34daaeffd002cf20459a4d24531
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-01-31 15:11:14 +0900

    6.1

--- init.js
+++ init.js
@@ -1,9 +1,13 @@
-import "./db";
+import "@babel/polyfill";
 import dotenv from "dotenv";
+import "./db";
 import app from "./app";
-import "./models/Video";
 
 dotenv.config();
+
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
